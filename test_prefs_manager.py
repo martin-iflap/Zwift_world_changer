2bda3e50b38b5af559963bdc1210f6a255e82e08
martin-iflap <martin.iflap@gmail.com>
2025-09-30 21:43:40 +0200

    Added test get current world

--- test_prefs_manager.py
+++ test_prefs_manager.py
@@ -1,10 +1,11 @@
 import pytest
 from pathlib import Path
 import xml.etree.ElementTree as Et
-
+from zwift_3 import ZwiftPrefsManager
 
 @pytest.fixture
 def prefs_file(tmp_path:Path):
+    """Creates temporary prefs file"""
     xml_path = tmp_path / "prefs.xml"
     root = Et.Element("ZWIFT")
     world = Et.SubElement(root, "WORLD")
@@ -13,3 +14,8 @@ def prefs_file(tmp_path:Path):
     tree.write(xml_path, encoding="utf-8", xml_declaration=True)
     return xml_path
 
+
+def test_get_current_world(prefs_file):
+    zpm = ZwiftPrefsManager()
+    zpm.set_prefs_file(str(prefs_file))
+    assert zpm.get_current_world() == 2
