8852f0589edc66edad2eefc9d37d3a89a19b3d79
martin-iflap <martin.iflap@gmail.com>
2025-10-01 17:22:00 +0200

    Added test_writes_file and 2 fixtures

--- test_prefs_manager.py
+++ test_prefs_manager.py
@@ -1,10 +1,10 @@
 import pytest
 from pathlib import Path
 import xml.etree.ElementTree as Et
-from zwift_3 import ZwiftPrefsManager
+import zwift_3
 
 @pytest.fixture
-def prefs_file(tmp_path:Path):
+def prefs_file(tmp_path:Path) -> Path:
     """Creates temporary prefs file"""
     xml_path = tmp_path / "prefs.xml"
     root = Et.Element("ZWIFT")
@@ -14,8 +14,24 @@ def prefs_file(tmp_path:Path):
     tree.write(xml_path, encoding="utf-8", xml_declaration=True)
     return xml_path
 
+@pytest.fixture
+def fake_mem(tmp_path, monkeypatch) -> Path:
+    """Creates a temporary memory.txt file"""
+    fake_mem = tmp_path / "memory.txt"
+    monkeypatch.setattr(zwift_3, "MEMORY", fake_mem)
+    return fake_mem
 
-def test_get_current_world(prefs_file):
-    zpm = ZwiftPrefsManager()
+@pytest.fixture
+def zpm() -> zwift_3.ZwiftPrefsManager:
+    return zwift_3.ZwiftPrefsManager()
+
+def test_get_current_world(prefs_file: Path, zpm, fake_mem: Path) -> None:
+    """Tests if the world is being read correctly from prefs.xml"""
     zpm.set_prefs_file(str(prefs_file))
     assert zpm.get_current_world() == 2
+
+def test_writes_file(fake_mem: Path, prefs_file: Path, zpm):
+    zpm.set_prefs_file(str(prefs_file))
+    assert fake_mem.read_text(encoding="utf-8").strip() == str(prefs_file.resolve())
+
+
